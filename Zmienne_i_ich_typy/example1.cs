2381765b917a90e432e53b7893c6efd39a838b7c
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2017-03-19 00:10:57 +0100

    Zmienne_i_ich_typy

---
+++ Zmienne_i_ich_typy/example1.cs
@@ -0,0 +1,12 @@
+namespace test1
+{ 
+      class Program
+      {
+          static void Main(string[] args)
+          {
+          int x=10;
+          Console.WriteLine(x);
+          Console.ReadKey();
+          }  
+      }   
+}
---
+++ Zmienne_i_ich_typy/example2.cs
@@ -0,0 +1,13 @@
+namespace test1
+{ 
+      class Program
+      {
+          static void Main(string[] args)
+          {
+          int x;
+          x=10;
+          Console.WriteLine(x);
+          Console.ReadKey();
+          }  
+      }   
+}
